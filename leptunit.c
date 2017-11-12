df8a5877f1d537fea8a4201144562dceca6d0824
Herbert Shen <ishbguy@hotmail.com>
2017-11-12 14:38:23 +0800

    Add GET_LIST_HEAD() and SET_LIST_HEAD() macros to clean code.

--- leptunit.c
+++ leptunit.c
@@ -7,6 +7,9 @@
 #include "xmacro.h"
 #include <assert.h>
 
+#define GET_LIST_HEAD(l, h)     h = (xlist_node_t *)(l)
+#define SET_LIST_HEAD(l, h)     l = (leptunit_list_t *)(h)
+
 typedef struct __xlist_node xlist_node_t;
 
 struct leptunit_list_t {
@@ -16,6 +19,7 @@ struct leptunit_list_t {
 void leptunit_init(leptunit_suit_t * suit)
 {
     assert(suit);
+
     suit->count = 0;
     suit->pass = 0;
     suit->tests = NULL;
@@ -25,29 +29,31 @@ void leptunit_add(leptunit_suit_t * suit, leptunit_t test)
 {
     assert(suit && test);
 
-    xlist_node_t *l = (xlist_node_t *) suit->tests;
+    xlist_node_t *l;
 
+    GET_LIST_HEAD(suit->tests, l);
     XLIST_ADD(l, test);
-    suit->tests = (leptunit_list_t *) l;
+    SET_LIST_HEAD(suit->tests, l);
 }
 
 void leptunit_clear(leptunit_suit_t * suit)
 {
     assert(suit);
 
-    xlist_node_t *l = (xlist_node_t *) suit->tests;
+    xlist_node_t *l;
 
+    GET_LIST_HEAD(suit->tests, l);
     XLIST_FREE(l);
-    suit->tests = NULL;
+    SET_LIST_HEAD(suit->tests, NULL);
 }
 
 void leptunit_run(leptunit_suit_t * suit)
 {
     assert(suit);
 
-    xlist_node_t *test;
-    xlist_node_t *l = (xlist_node_t *) suit->tests;
+    xlist_node_t *test, *l;
 
+    GET_LIST_HEAD(suit->tests, l);
     XLIST_FOR_EACH(test, l)
         ((leptunit_t) test->data) (suit);
 }
@@ -55,6 +61,7 @@ void leptunit_run(leptunit_suit_t * suit)
 int leptunit_summary(leptunit_suit_t * suit)
 {
     assert(suit);
+
     int fail = suit->count - suit->pass;
 
     printf("total: %d, pass: %d, fail: %d, (%3.2f%%).\n", suit->count,
